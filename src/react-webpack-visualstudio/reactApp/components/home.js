90bd2f8ed3db09eb3a7120af7f49bd5dfe173dfd
Maxime <richard.maxime9@gmail.com>
2016-10-16 22:32:11 +0200

    Appel /api/values dans react Utilisation de axios

--- src/react-webpack-visualstudio/reactApp/components/home.js
+++ src/react-webpack-visualstudio/reactApp/components/home.js
@@ -1,10 +1,30 @@
 ﻿import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Home extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            values: []
+        }
+
+        axios.get('/api/values')
+            .then(response => {
+                this.setState({ values: JSON.stringify(response.data) });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     render() {
-        return(
-            <h1>Hello, world!</h1>
+
+        return (
+            <div>
+                {this.state.values}
+           </div>
         );
     }
 
