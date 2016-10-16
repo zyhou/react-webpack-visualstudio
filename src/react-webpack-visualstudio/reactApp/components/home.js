d36bd807caceefcd157b1edae62ca6fb7f8613ac
Maxime <richard.maxime9@gmail.com>
2016-10-16 22:13:10 +0200

    Ajout components home

---
+++ src/react-webpack-visualstudio/reactApp/components/home.js
@@ -0,0 +1,11 @@
+﻿import React, { Component } from 'react';
+
+export default class Home extends Component {
+
+    render() {
+        return(
+            <h1>Hello, world!</h1>
+        );
+    }
+
+}
--- src/react-webpack-visualstudio/reactApp/index.js
+++ src/react-webpack-visualstudio/reactApp/index.js
@@ -1,5 +1,6 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
 
-let bonjour = <h1>Hello, world!</h1>
-ReactDOM.render(bonjour, document.getElementById('content'));
+import Home from './components/home';
+
+ReactDOM.render(<Home />, document.getElementById('content'));
